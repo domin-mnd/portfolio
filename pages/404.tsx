f3907f362495f1ed3306523a84eb41e59ce425ee
Domin-MND <dominus.the.creator@gmail.com>
2023-02-06 15:29:30 +0300

    loading for 404
    added loading overlay for 404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,10 +1,16 @@
+import { LoadingOverlay } from '@mantine/core';
 import { useRouter } from 'next/router';
-import { useEffect } from 'react';
+import { ReactElement, useEffect } from 'react';
 
 // A 404 page redirects to the home page
-export default () => {
+// Added loading overlay to prevent
+// loading times during development environment
+// as well as disabling header usage
+export default (): ReactElement => {
   const { push } = useRouter();
   useEffect(() => {
     push("/");
   }, []);
+
+  return <LoadingOverlay visible />;
 }
