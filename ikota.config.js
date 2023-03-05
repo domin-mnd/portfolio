3fa911a06eb38793ff190e4f39a13d803a7d18b8
Domin-MND <dominus.the.creator@gmail.com>
2023-03-05 20:58:27 +0300

    dogfeeding ikota
    added ikota configuration in portfolio

---
+++ ikota.config.js
@@ -0,0 +1,12 @@
+/**
+ * @type {import('ikota').IkotaConfig}
+ */
+module.exports = {
+  componentPath: "components",
+  useTypescript: true,
+  addConfigFile: true,
+  addIndexFile: true,
+  preprocessor: "css",
+  useLambdaSimplifier: false,
+  trailingSpace: true
+}
