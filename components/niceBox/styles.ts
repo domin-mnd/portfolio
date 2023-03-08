dcfd07526ac86bf412965dea1b492951d031b644
Domin-MND <dominus.the.creator@gmail.com>
2023-03-08 21:22:11 +0300

    added @ikota/mantine
    mantine preprocessor for ikota automation

--- components/niceBox/styles.ts
+++
@@ -1,7 +0,0 @@
-import { createStyles, MantineTheme } from "@mantine/core";
-
-export const useStyles = createStyles((theme: MantineTheme) => ({
-  card: {
-    backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[7] : theme.white,
-  }
-}));
