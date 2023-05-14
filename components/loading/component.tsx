e8532d958946c9ca19a822bcd7ce73d17918a17d
Domin-MND <dominus.the.creator@gmail.com>
2023-05-14 22:25:27 +0300

    correct naming

--- components/loading/component.tsx
+++ components/loading/component.tsx
@@ -24,17 +24,17 @@ export const Loading: FunctionComponent = (): ReactElement => {
             xmlns="http://www.w3.org/2000/svg"
             className={classes.animation}
           >
-            <g clip-path="url(#clip0_33_2)">
+            <g clipPath="url(#clip0_33_2)">
               <path
                 d="M12.1053 22.2105C17.6862 22.2105 22.2105 17.6862 22.2105 12.1053C22.2105 6.52428 17.6862 2 12.1053 2C6.52428 2 2 6.52428 2 12.1053C2 17.6862 6.52428 22.2105 12.1053 22.2105Z"
                 stroke="white"
-                stroke-opacity="0.5"
-                stroke-width="3.15789"
+                strokeOpacity={0.5}
+                strokeWidth={3.15789}
               />
               <path
                 d="M22.2107 12.1053C22.2107 5.82737 17.1202 2 12.1055 2"
                 stroke="white"
-                stroke-width="3.15789"
+                strokeWidth={3.15789}
               />
             </g>
             <defs>
